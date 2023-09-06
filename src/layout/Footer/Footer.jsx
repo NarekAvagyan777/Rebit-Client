aea2efa737475d33d0eada06b0f76930e103314c
Artur <yeggart1988@gmail.com>
2023-09-06 16:04:01 +0400

    adaptive

--- src/layout/Footer/Footer.jsx
+++ src/layout/Footer/Footer.jsx
@@ -16,7 +16,9 @@ export default function Footer() {
       <ContactUs />
       <div className={styles.container}>
         <div className={styles.content_wrapper}>
-          <div className={`${styles.content_wrapper__element} ${styles.text_part}`}>
+          <div
+            className={`${styles.content_wrapper__element} ${styles.text_part}`}
+          >
             <p>
               The text and graphic content of the website belongs to Rebit.ai
               and cannot be used by other resources without our permission and
@@ -24,59 +26,62 @@ export default function Footer() {
             </p>
             <p>© Rebit.ai 2023</p>
           </div>
+          <div style={{ display: 'flex', width: '100%',  }}>
+            <div
+              className={`${styles.content_wrapper__element} ${styles.links_block}`}
+            >
+              <p>
+                <a href="">Work</a>
+              </p>
+              <p>
+                <a href="">Services</a>
+              </p>
+              <p>
+                <a href="">About Us</a>
+              </p>
+              <p>
+                <a href="">Careers</a>
+              </p>
+            </div>
 
-          <div className={`${styles.content_wrapper__element} ${styles.links_block}`}>
-            <p>
-              <a href="">Work</a>
-            </p>
-            <p>
-              <a href="">Services</a>
-            </p>
-            <p>
-              <a href="">About Us</a>
-            </p>
-            <p>
-              <a href="">Careers</a>
-            </p>
-          </div>
+            <div
+              className={`${styles.content_wrapper__element} ${styles.links_block}`}
+            >
+              <p>
+                <a href="">Blog</a>
+              </p>
+              <p>
+                <a href="">Academy</a>
+              </p>
+              <p>
+                <a href="">Privacy Policy</a>
+              </p>
+            </div>
 
-          <div
-            className={`${styles.content_wrapper__element} ${styles.links_block}`}
-          >
-            <p>
-              <a href="">Blog</a>
-            </p>
-            <p>
-              <a href="">Academy</a>
-            </p>
-            <p>
-              <a href="">Privacy Policy</a>
-            </p>
-          </div>
-
-          <div
-            className={`${styles.content_wrapper__element} ${styles.contact_block}`}
-          >
-            <div className={styles.content_wrapper__icons_block}>
+            <div
+              className={`${styles.content_wrapper__element} ${styles.contact_block}`}
+            >
+              <div className={styles.content_wrapper__icons_block}>
                 <LinkedinIcon />
                 <FacebookIcon />
                 <TwitterIcon />
-              <a href="">
-                <i className="fa fa-twitter" aria-hidden="true"></i>
-              </a>
-            </div>
-            <div className={styles.content_wrapper__contact_block}>
-              <div>
-                <Image src={mail} alt='mail' />
-                <span>hello@rebit.ai</span>
+                <a href="">
+                  <i className="fa fa-twitter" aria-hidden="true"></i>
+                </a>
               </div>
-              <div>
-                <Image src={phone} alt='phone' />
-                <span>+374 77 701105</span>
-              </div>
-              <div>
-                <Image src={location} alt='location' />
-                <span>6, Tumanyan str, Yerevan, Armenia, 0001</span>
+              <div className={styles.content_wrapper__contact_block}>
+                <div>
+                  <Image src={mail} alt="mail" />
+                  <span>hello@rebit.ai</span>
+                </div>
+                <div>
+                  <Image src={phone} alt="phone" />
+                  <span>+374 77 701105</span>
+                </div>
+                <div>
+                  <Image src={location} alt="location" />
+                  <span>6, Tumanyan str, Yerevan, Armenia, 0001</span>
+                </div>
               </div>
             </div>
           </div>
