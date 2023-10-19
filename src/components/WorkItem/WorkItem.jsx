e7bfe26b328983e2fa518a45fba7bdef590653ef
Artur <yeggart1988@gmail.com>
2023-10-19 12:19:24 +0400

    commit

--- src/components/WorkItem/WorkItem.jsx
+++ src/components/WorkItem/WorkItem.jsx
@@ -1,26 +1,20 @@
-import Image from "next/image";
-import React from "react";
-import styles from "./WorkItem.module.scss";
+import Image from 'next/image'
+import React from 'react'
+import styles from './WorkItem.module.scss'
 
 function WorkItem({ works }) {
   return (
     <div className={styles.works_container}>
-      {works.map((work) => (
+      {works.map((work, index) => (
         <div className={styles.work_item} key={work.id}>
-          {/* <div style={{ display: 'flow', width: '100%' }}> */}
-          {/* <Image src={work.img} alt={work.title} width={520} /> */}
-          {/* </div> */}
-          <div
-            className={
-              work?.color === "green"
-                ? styles.dashBoard__green
-                : styles.dashBoard__blue
-            }
+          <div className={`${styles.dashBoard} ${work?.color === 'green' ? styles.gradient: ''} `}
+
+           
           >
             <Image
               src={work.img}
               alt={work.title}
-              // width={520}
+              width={520}
               className={styles.wotkImg}
             />
           </div>
@@ -31,7 +25,7 @@ function WorkItem({ works }) {
         </div>
       ))}
     </div>
-  );
+  )
 }
 
-export default WorkItem;
+export default WorkItem
